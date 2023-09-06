7a18bcfb131769020455b2014a2e092b1c8fac28
Marcin Prusinowski <marcin.prusinowski99@gmail.com>
2023-09-06 14:57:29 +0200

    Performance testing uncomment

--- src/performance_testing.py
+++ src/performance_testing.py
@@ -8,18 +8,18 @@ bellman_times = []
 floyd_times = []
 dijkstra_times = []
 
-# for size in range(100, 600, 100):
-#     currencies = generate_currencies(size)
-#     rates = generate_rates(currencies)
-#     rates_graph = build_graph(rates)
-#
-#     bellman_time = timeit(lambda: bellman_ford_arbitrage(rates_graph, currencies[0]), number=1)
-#     floyd_time = timeit(lambda: floyd_warshall_negative_cycle(rates_graph), number=1)
-#     dijkstra_time = timeit(lambda: dijkstra_arbitrage(rates_graph, currencies[0]), number=1)
-#
-#     bellman_times.append(bellman_time)
-#     floyd_times.append(floyd_time)
-#     dijkstra_times.append(dijkstra_time)
+for size in range(100, 600, 100):
+    currencies = generate_currencies(size)
+    rates = generate_rates(currencies)
+    rates_graph = build_graph(rates)
+
+    bellman_time = timeit(lambda: bellman_ford_arbitrage(rates_graph, currencies[0]), number=1)
+    floyd_time = timeit(lambda: floyd_warshall_negative_cycle(rates_graph), number=1)
+    dijkstra_time = timeit(lambda: dijkstra_arbitrage(rates_graph, currencies[0]), number=1)
+
+    bellman_times.append(bellman_time)
+    floyd_times.append(floyd_time)
+    dijkstra_times.append(dijkstra_time)
 
 dijkstra_times_2 = []
 floyd_times_2 = []
@@ -31,7 +31,7 @@ for size in range(10000, 15000, 1000):
     rates_graph = build_graph(rates)
 
 
-    # floyd_time = timeit(lambda: floyd_warshall_negative_cycle(rates_graph), number=1)
+    floyd_time = timeit(lambda: floyd_warshall_negative_cycle(rates_graph), number=1)
     dijkstra_time = timeit(lambda: dijkstra_arbitrage(rates_graph, currencies[0]), number=1)
 
     # floyd_times_2.append(floyd_time)
@@ -39,13 +39,13 @@ for size in range(10000, 15000, 1000):
 
 plt.figure(figsize=(10, 6))
 
-# plt.plot(range(100, 600, 100), bellman_times, label='Bellman-Ford (Small)')
-# plt.plot(range(100, 600, 100), floyd_times, label='Floyd-Warshall (Small)')
-# plt.plot(range(100, 600, 100), dijkstra_times, label='Dijkstra (Small)')
+plt.plot(range(100, 600, 100), bellman_times, label='Bellman-Ford (Small)')
+plt.plot(range(100, 600, 100), floyd_times, label='Floyd-Warshall (Small)')
+plt.plot(range(100, 600, 100), dijkstra_times, label='Dijkstra (Small)')
 
-# plt.plot(range(3000, 6000, 1000), floyd_times_2, label='Floyd-Warshall (Large)')
+plt.plot(range(3000, 6000, 1000), floyd_times_2, label='Floyd-Warshall (Large)')
 plt.plot(range(10000, 15000, 1000), dijkstra_times_2, label='Dijkstra (Large)')
-#
+
 plt.xlabel('Graph Size')
 plt.ylabel('Time (seconds)')
 plt.title('Algorithm Comparison')
